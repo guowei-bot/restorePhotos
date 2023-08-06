76251ea8297daf1e6c4c5319b285b990186de0bc
guowezhou01 <guowezhou01.com>
2023-08-06 09:48:03 +0800

    delete

--- pages/restore.tsx
+++ pages/restore.tsx
@@ -107,15 +107,7 @@ const Home: NextPage = () => {
 
       <Header photo={session?.user?.image || undefined} />
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-4 sm:mb-0 mb-8">
-        <a
-          href="https://twitter.com/nutlope/status/1626074563481051136"
-          target="_blank"
-          rel="noreferrer"
-          className="border rounded-2xl py-1 px-4 text-slate-500 text-sm mb-5 hover:text-slate-600 transition duration-300 ease-in-out"
-        >
-          <span className="font-semibold">647,143 images</span> restored and
-          counting
-        </a>
+
         <h1 className="mx-auto max-w-4xl font-display text-4xl font-bold tracking-normal text-slate-900 sm:text-6xl mb-5">
           Restore any face photo
         </h1>
